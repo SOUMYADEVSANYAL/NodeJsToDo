566719a080045d389c8e7c10b567344b78717822
Soumyadev Sanyal <soumya.sp35@gamil.com>
2024-02-16 12:38:15 +0530

    Route protection implemented.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,47 +1,55 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/user-schema');
 exports.getSignIn = (req, res, next) => {
-    res.render('login', {
-        isAuth: false,
-        path: '/login'
-    });
+    if (req.session.isLoggedIn) {
+        res.redirect('/error');
+    } else {
+        res.render('login', {
+            isAuth: false,
+            path: '/login'
+        });
+    }
 };
 
 exports.getSignUp = (req, res, next) => {
-    res.render('signup', {
-        isAuth: req.session.isLoggedIn,
-        path: '/signup'
-    });
+    if (req.session.isLoggedIn) {
+        res.redirect('/error');
+    } else {
+        res.render('signup', {
+            isAuth: req.session.isLoggedIn,
+            path: '/signup'
+        });
+    }
 };
 
 exports.postSignIn = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    User.findOne({email: email})
-    .then((user) => {
-        if(!user){
-            console.log('User not found');
-            res.redirect('/login');
-        }else{
-            bcrypt
-            .compare(password, user.password)
-            .then((doMatch)=>{
-                if(doMatch){
-                    req.session.isLoggedIn = true;
-                    req.session.user = user;
-                    console.log('Logged in Successfully');
-                    res.redirect('/');
-                }else{
-                    console.log('Password does not match');
-                    res.redirect('/login');
-                }
-            })
-        }
-    }).catch(err => {
-        console.log(err);
-        console.log('Something went wrong');
-        res.redirect('/error');
-    });
+    User.findOne({ email: email })
+        .then((user) => {
+            if (!user) {
+                console.log('User not found');
+                res.redirect('/login');
+            } else {
+                bcrypt
+                    .compare(password, user.password)
+                    .then((doMatch) => {
+                        if (doMatch) {
+                            req.session.isLoggedIn = true;
+                            req.session.user = user;
+                            console.log('Logged in Successfully');
+                            res.redirect('/');
+                        } else {
+                            console.log('Password does not match');
+                            res.redirect('/login');
+                        }
+                    })
+            }
+        }).catch(err => {
+            console.log(err);
+            console.log('Something went wrong');
+            res.redirect('/error');
+        });
 };
 
 exports.postSignUp = (req, res, next) => {
@@ -52,20 +60,20 @@ exports.postSignUp = (req, res, next) => {
         .then((existingUser) => {
             if (existingUser) {
                 res.redirect('/signup');
-            }else{
+            } else {
                 bcrypt
-                .hash(password, 12)
-                .then((hashedPassword)=>{
-                    const user = new User({
-                        name: name,
-                        email: email,
-                        password: hashedPassword
+                    .hash(password, 12)
+                    .then((hashedPassword) => {
+                        const user = new User({
+                            name: name,
+                            email: email,
+                            password: hashedPassword
+                        });
+                        user.save();
+                    })
+                    .then(() => {
+                        res.redirect('/login');
                     });
-                    user.save();
-                })
-                .then(()=>{
-                    res.redirect('/login');
-                });
             }
         })
         .catch(err => {
